2272be0d3003f0d25c82561e07b9ba02e5b6658c
Nick Meinhold <nick.meinhold@gmail.com>
2021-07-01 15:20:49 +1000

    Add googleapis

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,7 +1,19 @@
 import 'package:functions_framework/functions_framework.dart';
+import 'package:googleapis/docs/v1.dart' as docs_pkg;
+import 'package:googleapis/sheets/v4.dart' as sheets_pkg;
+import 'package:googleapis/slides/v1.dart' as slides_pkg;
+import 'package:googleapis_auth/auth_io.dart';
 import 'package:shelf/shelf.dart';
 
 @CloudFunction()
-Response function(Request request) {
+Future<Response> function(Request request) async {
+  final authClient = await clientViaApplicationDefaultCredentials(
+    scopes: [
+      docs_pkg.DocsApi.documentsReadonlyScope,
+      sheets_pkg.SheetsApi.spreadsheetsReadonlyScope,
+      slides_pkg.SlidesApi.presentationsReadonlyScope,
+    ],
+  );
+
   return Response.ok('Hello, World!');
 }
